52620c2a6fbd66c8fb062ba2dbc170bb80e35023
Jesus Rodriguez <foxandxss@gmail.com>
2014-09-03 21:25:03 +0200

    Add basic vendor and scss preprocessing

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,38 @@ var gulp          = require('gulp'),
     rimraf        = require('gulp-rimraf'),
     templateCache = require('gulp-angular-templatecache'),
     minifyHtml    = require('gulp-minify-html'),
-    es            = require('event-stream');
+    es            = require('event-stream'),
+    sass          = require('gulp-sass'),
+    rename        = require('gulp-rename');
 
 var paths = {
-  appJavascript: ['app/js/app.js', 'app/js/**/*.js'],
-  appTemplates:  'app/js/**/**.tpl.html',
-  tmpFolder:     '.tmp',
-  tmpJavascript: '.tmp/js'
+  appJavascript:    ['app/js/app.js', 'app/js/**/*.js'],
+  appTemplates:     'app/js/**/**.tpl.html',
+  appSass:          'app/scss/main.scss',
+  indexHtml:        'app/index.html',
+  vendorJavascript: ['vendor/js/angular.js', 'vendor/js/**/*.js'],
+  tmpFolder:        '.tmp',
+  tmpJavascript:    '.tmp/js',
+  tmpCss:           '.tmp/css'
 };
 
 gulp.task('scripts', function() {
-  return gulp.src(paths.appJavascript.concat(paths.appTemplates))
+  return gulp.src(paths.vendorJavascript.concat(paths.appJavascript, paths.appTemplates))
     .pipe(gulpif(/html$/, buildTemplates()))
     .pipe(concat('app.js'))
     .pipe(gulp.dest(paths.tmpJavascript));
+});
+
+gulp.task('styles', function() {
+  return gulp.src(paths.appSass)
+    .pipe(sass())
+    .pipe(rename('app.css'))
+    .pipe(gulp.dest(paths.tmpCss))
+});
+
+gulp.task('indexHtml', function() {
+  return gulp.src(paths.indexHtml)
+    .pipe(gulp.dest(paths.tmpFolder))
 });
 
 gulp.task('clean', function() {
